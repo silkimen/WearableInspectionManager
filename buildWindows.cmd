8cd6291b7aafb8e37a4044719572f2bdd1f654e0
Sefa Ilkimen <sefa.ilkimen@googlemail.com>
2014-04-20 17:25:54 +0200

    added custom icon for nw package

--- buildWindows.cmd
+++ buildWindows.cmd
@@ -3,5 +3,5 @@ if exist .\bin\ (rd /s /q .\bin)
 mkdir .\bin
 .\7zip\7za.exe a -tzip .\bin\app.pak .\package.json .\www
 robocopy .\nw\win\ .\bin\ *.dll nw.pak
-copy /b .\nw\win\nw.exe + .\bin\app.pak .\bin\inspectionManager.exe
+copy /b .\nw\win\nw_custom.exe + .\bin\app.pak .\bin\inspectionManager.exe
 del .\bin\app.pak /f /q
