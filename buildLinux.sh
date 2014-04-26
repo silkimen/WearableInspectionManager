e9cc878d35322fe0d68af1f32dba2e1e96137fde
silkimen <sefa.ilkimen@googlemail.com>
2014-04-26 17:49:31 +0200

    added linux build on linux machine

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,5 +1,36 @@
-﻿#!/bin/bash
+#!/bin/bash
+
+
+if [ -d ./tmp ]; then
+  rm -rf ./tmp
+fi
 
 if [ -d ./bin ]; then
   rm -rf ./bin
 fi
+
+mkdir ./tmp
+mkdir ./bin
+mkdir ./bin/win
+mkdir ./bin/linux_ia32
+mkdir ./bin/linux_x64
+
+cp -r ./www ./tmp/
+cp ./package.json ./tmp/
+cd tmp
+zip -r ../bin/app.pak *
+cd ..
+
+cp ./nw/win/*.dll ./bin/win/
+cp ./nw/win/nw.pak ./bin/win/
+cp ./nw/linux_ia32/* ./bin/linux_ia32/
+cp ./nw/linux_x64/* ./bin/linux_x64/
+
+cat ./nw/win/nw_custom.exe ./bin/app.pak > ./bin/win/inspectionManager.exe
+cat ./nw/linux_ia32/nw ./bin/app.pak > ./bin/linux_ia32/inspectionManager && chmod +x ./bin/linux_ia32/inspectionManager
+cat ./nw/linux_x64/nw ./bin/app.pak > ./bin/linux_x64/inspectionManager && chmod +x ./bin/linux_x64/inspectionManager
+
+rm -rf ./tmp
+rm ./bin/app.pak
+
+
